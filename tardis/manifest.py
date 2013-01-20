444c7e1cf581b28a78fdc54d6c54a1b2ca1c0389
Gordon Syme <gordonsyme@gmail.com>
2013-01-20 13:13:45 +0000

    Refactoring, doesn't pass tests

--- tardis/manifest.py
+++ tardis/manifest.py
@@ -1,5 +1,7 @@
 import os
 import os.path
+import pwd, grp, stat
+
 import logging
 import csv
 import functools
@@ -12,11 +14,13 @@ class ManifestEntry(object):
     """An entry in the backup manifest
 
     ManifestEntry objects maintain the relationship between a file name, the
-    sha1sum of its content and the S3 object ID naming the content tarball.
+    sha1sum of its content and the S3 object ID naming the content.
 
-    Note that the content tarball may not exist in S3 yet.
+    TODO insert comment about stat info        
+
+    Note that the content may not exist in S3 yet.
     """
-    def __init__(self, file_path, object_id=None):
+    def __init__(self, file_path, object_id=None, *stat_info):
         """Create a Manifest
 
         file_path - file name
@@ -31,13 +35,11 @@ class ManifestEntry(object):
 
         self.path = file_path
         self.object_id = object_id
-
-        if self.object_id is None:
-            checksum = self._checksum_for_file(file_path)
-            self.object_id = "data/{}/{}".format(sha1sum(os.path.basename(file_path)), checksum)
+        self.stat_info = stat_info
 
     def __repr__(self):
-        return "<ManifestEntry({}, {})>".format(repr(self.path), repr(self.object_id))
+        return "<ManifestEntry({!r}, {!r}, {!r}, {!r}, {!r}, {!r}, {!r}, {!r})>".format(
+                        self.path, self.object_id, *self.stat_info)
 
     @property
     def checksum(self):
@@ -47,41 +49,12 @@ class ManifestEntry(object):
         return self.checksum != other.checksum
 
     @classmethod
-    def _checksum_for_file(cls, path):
-        if not os.path.isfile(path):
-            raise ValueError("{} does not name a file".format(path))
-
-        with open(path, 'rb') as f:
-            parts = iter(functools.partial(f.read, 32768), '')
-            checksum = sha1sum(parts)
-
-            logging.debug("{} --> {}".format(path, checksum))
-
-        logging.debug("Content checksum: {}".format(checksum))
-
-        return checksum
-
-    @classmethod
-    def for_directory(cls, path):
-        logging.debug("Creating manifest entries for {}".format(path))
-
-        if not os.path.isdir(path):
-            raise ValueError("{} does not name a directory".format(path))
-
-        file_paths = filter(os.path.isfile, [os.path.join(path, f) for f in os.listdir(path)])
-
-        if not file_paths:
-            raise ValueError("{} contains no files".format(path))
-
-        return (cls(file_path) for file_path in sorted(file_paths))
-
-    @classmethod
     def from_fields(cls, fields):
         logging.debug("Creating ManifestEntry from: {}".format(fields))
         return cls(*fields)
 
-    def as_fields(self):
-        return (self.path, self.object_id)
+    def as_fields(self, base_path=''):
+        return (os.path.join(base_path, self.path), self.object_id) + self.stat_info
 
     def __eq__(self, other):
         if isinstance(other, ManifestEntry):
@@ -107,14 +80,23 @@ class NullManifestEntry(object):
 class Manifest(object):
     """A backup manifest.
 
+    <
+    Recursive structure:
+        - current directory
+        - ManifestEntries for each regular file
+        - Manifests for each subdirectory
+    >
+
     Maps file paths to the ManifestEntry for that file.
 
     Manifest keys are named in the following format:
 
     manifest/{hostname}/{user}/{timestamp}
     """
-    def __init__(self, entries, name=None):
-        self.entries = defaultdict(NullManifestEntry, entries)
+    def __init__(self, path, entries=None, manifests=None, name=None):
+        self.path = path
+        self.manifests = manifests
+        self.entries = entries
         self._name = name
 
     @property
@@ -144,11 +126,18 @@ class Manifest(object):
         return not result
 
     def to_csv(self, stream):
+        # Write the name of *this* manifest, but not the sub-manifests
+        # Each entry needs to know the path in order to properly write filenames
         logging.debug("Writing {} to csv, entries: {}".format(self.__class__, self.entries))
 
         writer = csv.writer(stream, delimiter=':', lineterminator='\n')
         writer.writerow([self.name])
-        writer.writerows(self.entries[entry].as_fields() for entry in self.entries)
+        self._to_csv(writer)
+
+    def _to_csv(self, writer):
+        writer.writerows(entry.as_fields(self.path) for entry in self.entries)
+        for submanifest in manifests:
+            submanifest._to_csv(writer)
 
     @classmethod
     def name_prefix_for(cls, hostname, user):
@@ -170,30 +159,64 @@ class Manifest(object):
         return cls({ entry.path : entry for entry in entries }, name)
 
     @classmethod
-    def for_directories(cls, hostname, user, directories, ignored_directories=None):
-        if ignored_directories is None:
+    def for_directory(cls, hostname, user, path, ignored_directories=None):
+        if not hostname:
+            raise ValueError("hostname must be a non-empty string")
+
+        if not user:
+            raise ValueError("user must be a non-empty string")
+
+        if not os.path.isdir(path):
+            raise ValueError("{} does not name a directory".format(path))
+
+        if not ignored_directories:
             ignored_directories = []
 
-        entries = {}
+        path = os.path.abspath(path)
 
-        queue = [backup_root for backup_root in directories if os.path.isdir(backup_root)]
+        logging.debug("Creating manifest for {}".format(path))
 
-        for directory in queue:
-            directory = os.path.abspath(directory)
-            logging.debug("Examining {}".format(directory))
+        if path not in ignored_directories:
+            paths = [ os.path.join(path, p) for p in os.listdir(path) ]
 
-            if directory not in ignored_directories:
-                try:
-                    for entry in ManifestEntry.for_directory(directory):
-                        entries[entry.path] = entry
-                        logging.debug(entry)
-                except ValueError as e:
-                    logging.exception(e)
+            entries = [ ManifestEntry(os.path.basename(f), cls._object_id(f), *cls._stat_info(f))
+                            for f in paths if os.path.isfile(f) ]
 
-                # ewww
-                subdirectories = [os.path.join(directory, subpath) for subpath in os.listdir(directory)]
-                subdirectories = [subdir for subdir in subdirectories if os.path.isdir(subdir)]
+            manifests = [ cls.for_directory(hostname, user, d, ignored_directories)
+                            for d in paths if os.path.isdir(d) ]
 
-                queue.extend(sorted(subdirectories))
+            return cls(path, entries, manifests, cls.name_for(hostname, user))
 
-        return cls(entries, cls.name_for(hostname, user))
+    @classmethod
+    def _object_id(cls, path):
+        checksum = cls._checksum_for_file(path)
+        return "data/{}/{}".format(sha1sum(os.path.basename(path)), checksum)
+
+    @classmethod
+    def _checksum_for_file(cls, path):
+        if not os.path.isfile(path):
+            raise ValueError("{} does not name a file".format(path))
+
+        with open(path, 'rb') as f:
+            parts = iter(functools.partial(f.read, 32768), '')
+            checksum = sha1sum(parts)
+
+            logging.debug("{} --> {}".format(path, checksum))
+
+        logging.debug("Content checksum: {}".format(checksum))
+
+        return checksum
+
+    @classmethod
+    def _stat_info(cls, path):
+        stat_struct = os.stat(path)
+
+        owner = pwd.getpwuid(stat_struct.st_uid).pw_name
+        group = grp.getgrgid(stat_struct.st_gid).gr_name
+        mode  = stat.S_IMODE(stat_struct.st_mode)
+
+        ctime = os.path.getctime(path)
+        mtime = os.path.getmtime(path)
+        size  = os.path.getsize(path)
+
+        return (owner, group, mode, int(ctime), int(mtime), size)
